6809513a0f24189e97268b13a1b867fdf6d43e86
vishnu4reddy <vreddy@msystechnologies.com>
2023-08-22 15:26:58 +0530

    done

--- Tests/test_codespace.py
+++ Tests/test_codespace.py
@@ -1,17 +1,17 @@
-# # from Libraries.github import LoginPage
-# # from playwright.sync_api import Page
-# # from Data import config
-# # Email = config.user_git
-# # password = config.git_pass
+from Libraries.github import LoginPage
+from playwright.sync_api import Page
+from Data import config
+Email = config.user_git
+password = config.git_pass
 
 
-# # def test_check_login(page: Page):
-# #     login_page = LoginPage(page)
-# #     login_page.navigate()
-# #     login_page.login(Email, password)
-# #     login_page.repositories()
-# #     login_page.test_files()
-# #     login_page.Codespaces()
+def test_check_login(page: Page):
+    login_page = LoginPage(page)
+    login_page.navigate()
+    login_page.login(Email, password)
+    login_page.repositories()
+    login_page.test_files()
+    login_page.Codespaces()
 
 # from selenium import webdriver
 # import time
@@ -70,86 +70,86 @@
 # driver.close()
 # print("end")
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
-import requests
-print("Start")
-email = "ymudigeti@msystechnologies.com"
-password = "Yaminimudigeti2@"
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# import time
+# import requests
+# print("Start")
+# email = "ymudigeti@msystechnologies.com"
+# password = "Yaminimudigeti2@"
 
-# Set up the Selenium webdriver
-driver = webdriver.Chrome()
+# # Set up the Selenium webdriver
+# driver = webdriver.Chrome()
 
-# Navigate to the page with captcha
-driver.get("https://www.google.com/")
-time.sleep(3)
-dropdown = driver.find_element(By.XPATH, "//textarea[@id='APjFqb']")
-dropdown.click()
-dropdown.send_keys("keka login")
-dropdown.send_keys(Keys.ENTER)
-time.sleep(4)
-driver.find_element(By.XPATH, "//h3[normalize-space()='Keka Login']").click()
-time.sleep(4)
-driver.find_element(By.XPATH, "//input[@id='email']").send_keys(email)
-time.sleep(5)
-driver.find_element(By.XPATH, "//button[@type='submit']").click()
-time.sleep(3)
-driver.find_element(By.XPATH, "//p[normalize-space()='Keka Password']").click()
-time.sleep(2)
+# # Navigate to the page with captcha
+# driver.get("https://www.google.com/")
+# time.sleep(3)
+# dropdown = driver.find_element(By.XPATH, "//textarea[@id='APjFqb']")
+# dropdown.click()
+# dropdown.send_keys("keka login")
+# dropdown.send_keys(Keys.ENTER)
+# time.sleep(4)
+# driver.find_element(By.XPATH, "//h3[normalize-space()='Keka Login']").click()
+# time.sleep(4)
+# driver.find_element(By.XPATH, "//input[@id='email']").send_keys(email)
+# time.sleep(5)
+# driver.find_element(By.XPATH, "//button[@type='submit']").click()
+# time.sleep(3)
+# driver.find_element(By.XPATH, "//p[normalize-space()='Keka Password']").click()
+# time.sleep(2)
 
 
-# Assuming the captcha image is identified by a specific CSS selector
-captcha_img = driver.find_element(By.XPATH, "//img[@id='imgCaptcha']")
+# # Assuming the captcha image is identified by a specific CSS selector
+# captcha_img = driver.find_element(By.XPATH, "//img[@id='imgCaptcha']")
 
-# Get the URL of the captcha image
-captcha_img_url = captcha_img.get_attribute('src')
+# # Get the URL of the captcha image
+# captcha_img_url = captcha_img.get_attribute('src')
 
-# Download the captcha image
-captcha_img_response = requests.get(captcha_img_url, stream=True)
-with open('captcha_image.png', 'wb') as img_file:
-    img_file.write(captcha_img_response.content)
+# # Download the captcha image
+# captcha_img_response = requests.get(captcha_img_url, stream=True)
+# with open('captcha_image.png', 'wb') as img_file:
+#     img_file.write(captcha_img_response.content)
 
-# Use Ant i-Captcha API to solve the captcha
-anti_captcha_api_key = 'your_anti_captcha_api_key'
-captcha_solver_url = f'http://api.anti-captcha.com/createTask'
-captcha_solver_data = {
-    'clientKey': anti_captcha_api_key,
-    'task': {
-        'type': 'ImageToTextTask',
-        'body': captcha_img_response.content,
-        'phrase': False,
-        'case': False,
-        'numeric': 0,
-        'math': False,
-        'minLength': 0,
-        'maxLength': 0
-    }
-}
+# # Use Ant i-Captcha API to solve the captcha
+# anti_captcha_api_key = 'your_anti_captcha_api_key'
+# captcha_solver_url = f'http://api.anti-captcha.com/createTask'
+# captcha_solver_data = {
+#     'clientKey': anti_captcha_api_key,
+#     'task': {
+#         'type': 'ImageToTextTask',
+#         'body': captcha_img_response.content,
+#         'phrase': False,
+#         'case': False,
+#         'numeric': 0,
+#         'math': False,
+#         'minLength': 0,
+#         'maxLength': 0
+#     }
+# }
 
-response = requests.post(captcha_solver_url, json=captcha_solver_data)
-task_result = response.json()
+# response = requests.post(captcha_solver_url, json=captcha_solver_data)
+# task_result = response.json()
 
-# Get the task ID from the response
-task_id = task_result['taskId']
+# # Get the task ID from the response
+# task_id = task_result['taskId']
 
-# Poll the Anti-Captcha API for captcha solution
-while True:
-    time.sleep(5)
-    solution_response = requests.get(f'http://api.anti-captcha.com/getTaskResult?clientKey={anti_captcha_api_key}&taskId={task_id}')
-    solution_result = solution_response.json()
+# # Poll the Anti-Captcha API for captcha solution
+# while True:
+#     time.sleep(5)
+#     solution_response = requests.get(f'http://api.anti-captcha.com/getTaskResult?clientKey={anti_captcha_api_key}&taskId={task_id}')
+#     solution_result = solution_response.json()
 
-    if solution_result['status'] == 'ready':
-        captcha_solution = solution_result['solution']['text']
-        break
+#     if solution_result['status'] == 'ready':
+#         captcha_solution = solution_result['solution']['text']
+#         break
 
-# Enter the captcha solution in the input field
-captcha_input = driver.find_element(By.ID, 'captcha_input')
-captcha_input.send_keys(captcha_solution)
+# # Enter the captcha solution in the input field
+# captcha_input = driver.find_element(By.ID, 'captcha_input')
+# captcha_input.send_keys(captcha_solution)
 
-# Continue with the rest of your automation
-# ...
+# # Continue with the rest of your automation
+# # ...
 
-# Close the webdriver
-driver.quit()
+# # Close the webdriver
+# driver.quit()
