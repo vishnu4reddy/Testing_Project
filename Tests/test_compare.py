42f530149a28f8612ffb7036496cd91aface23e1
vishnu4reddy <113615826+vishnu4reddy@users.noreply.github.com>
2025-04-30 12:19:38 +0530

    Update test_compare.py

--- Tests/test_compare.py
+++ Tests/test_compare.py
@@ -2,21 +2,21 @@ import pytest
 
 
 @pytest.mark.xfail
-@pytest.mark.another_custom_marker
+
 def test_greater():
     num = 100
     assert num > 100
 
 
 @pytest.mark.xfail
-@pytest.mark.greatest_of_all_time
+
 def test_greater_equal():
     num = 100
     assert num >= 100
 
 
 @pytest.mark.skip
-@pytest.mark.custom_poraraiii
+
 def test_less():
     num = 100
     assert num < 200
